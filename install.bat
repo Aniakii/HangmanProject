5b5193b84b17fed66956e70dcfa760eb2902637f
Aniakii <95714597+Aniakii@users.noreply.github.com>
2023-06-21 14:09:02 +0200

    Delete install.bat

--- install.bat
+++
@@ -1,8 +0,0 @@
-git clone https://github.com/Jakub-Nnoli/HangmanProject.git
-cd HangmanProject
-del install.bat
-del README.md
-git remote remove origin
-rmdir /s /q .git
-pip install -r requirements.txt
-python main.py
